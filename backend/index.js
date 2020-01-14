b2edf6da411a5b715da9b84adb7775be09cc8158
Hugo Ribeiro <hgribeirolive@gmail.com>
2020-01-14 18:28:04 -0300

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/' , (request, response) =>{
+    return response.json({message: 'Hello world!'});
+} );
+
+app.listen(3333);
